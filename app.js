3a29150179d881eea0edeab0b2dcf746a3d391d9
Juan Morales <juanguillermo93@gmail.com>
2023-05-11 11:51:33 -0500

    Configuración del servidor

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+    if (res.headSent) {
+        return next(error)
+    }
+
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error ocurred!'})
+});
+
+app.listen(5000, console.log("Server running in port 5000"))
